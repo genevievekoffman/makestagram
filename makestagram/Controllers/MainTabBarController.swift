fae61fbdd39c3675a7dda201ad51d1ff632f6153
genevievekoffman <koffmg@blair.edu>
2017-07-21 11:29:58 -0400

    update

---
+++ makestagram/Controllers/MainTabBarController.swift
@@ -0,0 +1,26 @@
+//
+//  MainTabBarController.swift
+//  makestagram
+//
+//  Created by Genevieve Koffman on 7/21/17.
+//  Copyright © 2017 Genevieve Koffman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        delegate = self
+        
+        tabBar.unselectedItemTintColor = .black
+    }
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        return true
+    }
+}
