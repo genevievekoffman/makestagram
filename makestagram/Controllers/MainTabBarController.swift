08cca999f3085a8cd87101583775dae9b99572e5
genevievekoffman <koffmg@blair.edu>
2017-07-25 11:53:17 -0400

    update

--- makestagram/Controllers/MainTabBarController.swift
+++ makestagram/Controllers/MainTabBarController.swift
@@ -9,28 +9,28 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-    let photoHelper = MGPhotoHelper()
+    let photoHelper = MGPhotoHelper()  // creates instance MGPhotoHelper object
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         photoHelper.completionHandler = { image in
-            PostService.create(for: image)
+            PostService.create(for: image)  //sets a closure. Whenever MGPhotoHelper recieves an image, calls this closure
         }
         
         delegate = self
         
         tabBar.unselectedItemTintColor = .black
-    } // calls on itself, takes the image and makes it black 0.0
+    } // sets the MainTabBarController as the delegate of its tab bar, set its color to black 0.0
 }
 
 extension MainTabBarController: UITabBarControllerDelegate {
+    
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
         if viewController.tabBarItem.tag == 1 {
-            // present photo taking action sheet
-            photoHelper.presentActionSheet(from: self)
+            photoHelper.presentActionSheet(from: self) //presents the action sheet from photoHelper, allows user to take a photo or upload a photo
             return false
         } else {
-            return true    }
+            return true    } // returns bool value that determines if the tab bar should present the selected UI view controller. if false, view controller will not be displayed.
     }
 }
--- makestagram/Helpers/MGPhotoHelper.swift
+++ makestagram/Helpers/MGPhotoHelper.swift
@@ -8,43 +8,41 @@
 
 import UIKit
 
-class MGPhotoHelper: NSObject {
+class MGPhotoHelper: NSObject { //when user has a image selected
     var completionHandler: ((UIImage) -> Void)?
     
     
      func presentActionSheet(from viewController: UIViewController) {
         let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .actionSheet)
-        
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+
+        if UIImagePickerController.isSourceTypeAvailable(.camera) { //checks if device has a camera
             
             let capturePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: { [unowned self] action in
                 self.presentImagePickerController(with: .camera, from: viewController)
             })
-            alertController.addAction(capturePhotoAction)
+            alertController.addAction(capturePhotoAction) // adds the action to alertController
         }
         
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let uploadAction = UIAlertAction(title: "Upload from Library", style: .default, handler: { [unowned self] action in
                 self.presentImagePickerController(with: .photoLibrary, from: viewController)
-                })
+                }) // performs previous steps for users photo library
             alertController.addAction(uploadAction)
         }
-        // 6
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alertController.addAction(cancelAction)
+        alertController.addAction(cancelAction) // adds cancel action
         
-        // 7
-        viewController.present(alertController, animated: true)
-    }
+        viewController.present(alertController, animated: true) //Dont understand line 
+        
+    } // this func initializes a new UIAlertController of type actionsheet(popup that will be displayed at bottom of screen) if the device has a camera available -> Creates a new UIAlertAction. Add the action to alertController instance(we just created) also does this for users library. cancel actions allows user to close UIAlertController.
     
     func presentImagePickerController(with sourceType: UIImagePickerControllerSourceType, from viewController: UIViewController) {
         let imagePickerController = UIImagePickerController()
-        imagePickerController.sourceType = sourceType
+        imagePickerController.sourceType = sourceType // determines whether UIImagePickerController will activate to take photo or upload photo
         imagePickerController.delegate = self
         
         viewController.present(imagePickerController, animated: true)
-    }
+    } //creates new instance of UIImagePickerController, this object presents a native UI component (allows user to take photo or choose image) Presents view controller. -- we must implement the protocols because we are the delegate
 }
 
 extension MGPhotoHelper: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
@@ -53,11 +51,12 @@ extension MGPhotoHelper: UINavigationControllerDelegate, UIImagePickerController
             completionHandler?(selectedImage)
         }
         
-        picker.dismiss(animated: true)
+        picker.dismiss(animated: true) // hides the image picker controller
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true)
     }
 }
-
+// two delegation methods: one is called when image is selected, other is called when cancel is tapped
+// didFinishPickingMediaWithInfo info: [String : Any] --> check if passed an image in dictionary, if there is an image it is passed to completionHandler 
