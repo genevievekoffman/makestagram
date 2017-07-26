25cfd8248a8cc18b0cc06595a1c685e68929bd4b
genevievekoffman <koffmg@blair.edu>
2017-07-26 14:47:33 -0400

    update

---
+++ makestagram/Views/PostHeaderCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostHeaderCell.swift
+//  makestagram
+//
+//  Created by Genevieve Koffman on 7/26/17.
+//  Copyright © 2017 Genevieve Koffman. All rights reserved.
+//
+
+import UIKit
+
+class PostHeaderCell: UITableViewCell {
+    @IBOutlet weak var usernameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    @IBAction func optionsButtonTapped(_ sender: UIButton) {
+         print("options button tapped")
+    }
+}
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+//
+//}
